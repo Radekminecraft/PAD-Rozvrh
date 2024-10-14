4fa90d02e4c28b1d46e4576784935149cdc6395f
Radekminecraft <radomirvondra@gmail.com>
2024-10-14 17:20:11 +0200

    brainrot

---
+++ Class.py
@@ -0,0 +1,5 @@
+class Class:
+    def __init__(self, timeStart, timeEnd, name):
+        self.timeStart = timeStart
+        self.timeEnd = timeEnd
+        self.name = name
---
+++ main.py
@@ -0,0 +1,40 @@
+from Class import Class
+from unidecode import unidecode
+def getDayByNumber(number):
+    days = ["pondeli", "utery", "streda", "ctvrtek", "patek"]
+    return days[number]
+def getClasses(file):
+    days = {}
+    lines = file.readlines()
+    for line in range(len(lines)):
+        classes = lines[line].strip().split(";")
+        dayClasses = []
+        for clazz in classes:
+            clazzSplit = clazz.split("-")
+            name = clazzSplit[0]
+            time = clazzSplit[1].split("|")
+            dayClasses.append(Class(time[0], time[1], name))
+        days[getDayByNumber(line)] = dayClasses
+    return days
+def getClassByDate(day, hour, minute, classes):
+    #Day, time format D/HH/MM
+    time = int(str(hour) + str(minute))
+    clazzez = classes.get(day)
+    if(clazzez == None):
+        return "Zadej den mezi pondělím a pátkem!"
+    for clazz in clazzez:
+        if time >= int(clazz.timeStart) and time <= int(clazz.timeEnd):
+            return clazz
+    if time < int(clazzez[0].timeStart) or time > int(clazzez[len(clazzez)-1].timeEnd):
+        return "Není škola"
+    return "Přestávka"
+rozvrh = open("rozvrh.txt", "r")
+classes = getClasses(rozvrh)
+day = unidecode(input("Zadej den (pondělí - pátek): ")).lower()
+time = input("Zadej čas (HH:MM): ")
+timeSplit = time.split(":")
+clazz = getClassByDate(day, timeSplit[0], timeSplit[1], classes)
+if isinstance(clazz, Class):
+    print(clazz.name)
+else:
+    print(clazz)
